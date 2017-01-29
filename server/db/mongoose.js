94087e3c2fdb83f67ec306471a74bf2635659405
Sotiris Valogiannis <omiloparmenos@gmail.com>
2017-01-30 00:44:17 +0200

    Setup seperate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,6 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://sotiris:35223522@ds137139.mlab.com:37139/node-js-lesson'
-};
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect( process.env.MONGODB_URI );
 
 module.exports = { mongoose };
