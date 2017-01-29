c083056724caf8b7b3cba103c55d575cd577eace
Sotiris Valogiannis <omiloparmenos@gmail.com>
2017-01-30 00:47:57 +0200

    Fix Heroku database URL

--- server/config/config.js
+++ server/config/config.js
@@ -7,4 +7,6 @@ if( env === 'development') {
 } else if( env === 'test') {
   process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if( env === 'production' ) {
+  process.env.MONGODB_URI = 'mongodb://sotiris:35223522@ds137139.mlab.com:37139/node-js-lesson'
 }
