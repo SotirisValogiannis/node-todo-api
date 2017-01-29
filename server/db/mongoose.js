4fb1959b222d3274bed43f66a0a738f67337bab2
Sotiris Valogiannis <omiloparmenos@gmail.com>
2017-01-29 19:54:40 +0200

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://sotiris:35223522@ds137139.mlab.com:37139/node-js-lesson'
+};
+mongoose.connect( db.localhost || db.mlab);
 
 module.exports = { mongoose };
